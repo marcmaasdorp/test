972fec303add06fe37f6fb2d1a92e2f95d8b9993
marcmaasdorp <marc@jamiicities.co.za>
2021-03-21 11:19:01 +0200

    Fixes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -8,6 +8,7 @@ from resources.store import Store, StoreList
 import os
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.secret_key = 'marc'
@@ -25,4 +26,4 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__=='__main__': 
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
--- models/item.py
+++ models/item.py
@@ -1,5 +1,4 @@
 from db import db
-from models.store import StoreModel
 
 class ItemModel(db.Model):
     __tablename__ = 'items'
